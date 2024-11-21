ffecfe48de2f9df8474f9a8171572f9c31f54774
shahryarsaeed23 <151502954+shahryarsaeed23@users.noreply.github.com>
2024-11-21 16:44:43 +0500

    cova diagnostic report changes

--- app/Imports/CovaDiagnosticReportImport.php
+++ app/Imports/CovaDiagnosticReportImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\CovaDiagnosticReport;
+use App\Models\Report;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Facades\Log;
@@ -22,33 +23,33 @@ class CovaDiagnosticReportImport implements ToModel, WithHeadingRow
         $this->location = $location;
         $this->reportId = $reportId;
         $this->retailerId = $retailerId; // Assign retailer ID
-        $this->lpId = $lpId; 
+        $this->lpId = $lpId;
     }
 
     public function model(array $row)
     {
         // List of required headers
         $requiredHeaders = [
-            'product_name', 
-            'type', 
-            'aglc_sku', 
-            'new_brunswick_sku', 
-            'ocs_sku', 
-            'ylc_sku', 
-            'manitoba_barcodeupc', 
-            'ontario_barcodeupc', 
-            'saskatchewan_barcodeupc', 
-            'link_to_product', 
-            'opening_inventory_units', 
-            'quantity_purchased_units', 
-            'reductions_receiving_error_units', 
-            'returns_from_customers_units', 
-            'other_additions_units', 
-            'quantity_sold_units', 
-            'quantity_destroyed_units', 
-            'quantity_lost_theft_units', 
-            'returns_to_supplier_units', 
-            'other_reductions_units', 
+            'product_name',
+            'type',
+            'aglc_sku',
+            'new_brunswick_sku',
+            'ocs_sku',
+            'ylc_sku',
+            'manitoba_barcodeupc',
+            'ontario_barcodeupc',
+            'saskatchewan_barcodeupc',
+            'link_to_product',
+            'opening_inventory_units',
+            'quantity_purchased_units',
+            'reductions_receiving_error_units',
+            'returns_from_customers_units',
+            'other_additions_units',
+            'quantity_sold_units',
+            'quantity_destroyed_units',
+            'quantity_lost_theft_units',
+            'returns_to_supplier_units',
+            'other_reductions_units',
             'closing_inventory_units'
         ];
 
@@ -60,13 +61,13 @@ class CovaDiagnosticReportImport implements ToModel, WithHeadingRow
                 $formattedHeaders = array_map(function ($header) {
                     return str_replace('_', ' ', $header); // Format headers for display
                 }, $missingHeaders);
-        
+
                 // Log the error with the raw header names
                 Log::error('Missing headers in CovaDiagnosticReport import: ' . implode(', ', $missingHeaders));
-        
+
                 // Store the formatted headers in the errors array
                 $this->errors[] = 'Missing header: ' . implode(', ', $formattedHeaders);
-        
+
                 // Throw an exception with the collected errors
                 throw new \Exception('' . implode(', ', $this->errors));
             }
@@ -75,36 +76,150 @@ class CovaDiagnosticReportImport implements ToModel, WithHeadingRow
             $this->hasCheckedHeaders = true;
         }
 
-        // Proceed with creating the model if headers are valid
-        $diagnosticReport = new CovaDiagnosticReport([
-            'product_name' => $row['product_name'] ?? null,
-            'type' => $row['type'] ?? null,
-            'aglc_sku' => $row['aglc_sku'] ?? null,
-            'new_brunswick_sku' => $row['new_brunswick_sku'] ?? null,
-            'ocs_sku' => $row['ocs_sku'] ?? null,
-            'ylc_sku' => $row['ylc_sku'] ?? null,
-            'manitoba_barcodeupc' => $row['manitoba_barcodeupc'] ?? null,
-            'ontario_barcodeupc' => $row['ontario_barcodeupc'] ?? null,
-            'saskatchewan_barcodeupc' => $row['saskatchewan_barcodeupc'] ?? null,
-            'link_to_product' => $row['link_to_product'] ?? null,
-            'opening_inventory_units' => $row['opening_inventory_units'] ?? null,
-            'quantity_purchased_units' => $row['quantity_purchased_units'] ?? null,
-            'reductions_receiving_error_units' => $row['reductions_receiving_error_units'] ?? null,
-            'returns_from_customers_units' => $row['returns_from_customers_units'] ?? null,
-            'other_additions_units' => $row['other_additions_units'] ?? null,
-            'quantity_sold_units' => $row['quantity_sold_units'] ?? null,
-            'quantity_destroyed_units' => $row['quantity_destroyed_units'] ?? null,
-            'quantity_lost_theft_units' => $row['quantity_lost_theft_units'] ?? null,
-            'returns_to_supplier_units' => $row['returns_to_supplier_units'] ?? null,
-            'other_reductions_units' => $row['other_reductions_units'] ?? null,
-            'closing_inventory_units' => $row['closing_inventory_units'] ?? null,
-            'report_id' => $this->reportId,
-            'location' => $this->location,
-            'retailer_id' => $this->retailerId, // Include retailer ID
-            'lp_id' => $this->lpId,
-        ]);
+        $report = Report::where('id',$this->reportId)->first();
+        if ($report->province == 'ON' || $report->province == 'Ontario') {
+            if ($row['ocs_sku'] != null || $row['product_name'] != null || $row['ontario_barcode_upc'] != null) {
+                if (($row['ocs_sku'] != '*')) {
+                    $diagnosticReport = new CovaDiagnosticReport([
+                        'product_name' => $row['product_name'] ?? null,
+                        'type' => $row['type'] ?? null,
+                        'aglc_sku' => $row['aglc_sku'] ?? null,
+                        'new_brunswick_sku' => $row['new_brunswick_sku'] ?? null,
+                        'ocs_sku' => $row['ocs_sku'] ?? null,
+                        'ylc_sku' => $row['ylc_sku'] ?? null,
+                        'manitoba_barcodeupc' => $row['manitoba_barcodeupc'] ?? null,
+                        'ontario_barcodeupc' => $row['ontario_barcodeupc'] ?? null,
+                        'saskatchewan_barcodeupc' => $row['saskatchewan_barcodeupc'] ?? null,
+                        'link_to_product' => $row['link_to_product'] ?? null,
+                        'opening_inventory_units' => $row['opening_inventory_units'] ?? null,
+                        'quantity_purchased_units' => $row['quantity_purchased_units'] ?? null,
+                        'reductions_receiving_error_units' => $row['reductions_receiving_error_units'] ?? null,
+                        'returns_from_customers_units' => $row['returns_from_customers_units'] ?? null,
+                        'other_additions_units' => $row['other_additions_units'] ?? null,
+                        'quantity_sold_units' => $row['quantity_sold_units'] ?? null,
+                        'quantity_destroyed_units' => $row['quantity_destroyed_units'] ?? null,
+                        'quantity_lost_theft_units' => $row['quantity_lost_theft_units'] ?? null,
+                        'returns_to_supplier_units' => $row['returns_to_supplier_units'] ?? null,
+                        'other_reductions_units' => $row['other_reductions_units'] ?? null,
+                        'closing_inventory_units' => $row['closing_inventory_units'] ?? null,
+                        'report_id' => $this->reportId,
+                        'location' => $this->location,
+                        'retailer_id' => $this->retailerId, // Include retailer ID
+                        'lp_id' => $this->lpId,
+                    ]);
 
-        $diagnosticReport->save(); // Save to get the ID
+                    $diagnosticReport->save(); // Save to get the ID
+                }
+            }
+        }
+
+        if ($report->province == 'MB' || $report->province == 'Manitoba' || $report->province == 'BC' || $report->province == 'British Columbia') {
+            if ($row['new_brunswick_sku'] != null || $row['product_name'] != null || $row['manitoba_barcode_upc'] != null) {
+                if (($row['new_brunswick_sku'] != '*')) {
+                    $diagnosticReport = new CovaDiagnosticReport([
+                        'product_name' => $row['product_name'] ?? null,
+                        'type' => $row['type'] ?? null,
+                        'aglc_sku' => $row['aglc_sku'] ?? null,
+                        'new_brunswick_sku' => $row['new_brunswick_sku'] ?? null,
+                        'ocs_sku' => $row['ocs_sku'] ?? null,
+                        'ylc_sku' => $row['ylc_sku'] ?? null,
+                        'manitoba_barcodeupc' => $row['manitoba_barcodeupc'] ?? null,
+                        'ontario_barcodeupc' => $row['ontario_barcodeupc'] ?? null,
+                        'saskatchewan_barcodeupc' => $row['saskatchewan_barcodeupc'] ?? null,
+                        'link_to_product' => $row['link_to_product'] ?? null,
+                        'opening_inventory_units' => $row['opening_inventory_units'] ?? null,
+                        'quantity_purchased_units' => $row['quantity_purchased_units'] ?? null,
+                        'reductions_receiving_error_units' => $row['reductions_receiving_error_units'] ?? null,
+                        'returns_from_customers_units' => $row['returns_from_customers_units'] ?? null,
+                        'other_additions_units' => $row['other_additions_units'] ?? null,
+                        'quantity_sold_units' => $row['quantity_sold_units'] ?? null,
+                        'quantity_destroyed_units' => $row['quantity_destroyed_units'] ?? null,
+                        'quantity_lost_theft_units' => $row['quantity_lost_theft_units'] ?? null,
+                        'returns_to_supplier_units' => $row['returns_to_supplier_units'] ?? null,
+                        'other_reductions_units' => $row['other_reductions_units'] ?? null,
+                        'closing_inventory_units' => $row['closing_inventory_units'] ?? null,
+                        'report_id' => $this->reportId,
+                        'location' => $this->location,
+                        'retailer_id' => $this->retailerId, // Include retailer ID
+                        'lp_id' => $this->lpId,
+                    ]);
+
+                    $diagnosticReport->save(); // Save to get the ID
+                }
+            }
+        }
+
+        if ($report->province == 'SK' || $report->province == 'Saskatchewan') {
+            if ($row['ylc_sku'] != null || $row['product_name'] != null || $row['saskatchewan_barcode_upc'] != null) {
+                if (($row['ylc_sku'] != '*')) {
+                    $diagnosticReport = new CovaDiagnosticReport([
+                        'product_name' => $row['product_name'] ?? null,
+                        'type' => $row['type'] ?? null,
+                        'aglc_sku' => $row['aglc_sku'] ?? null,
+                        'new_brunswick_sku' => $row['new_brunswick_sku'] ?? null,
+                        'ocs_sku' => $row['ocs_sku'] ?? null,
+                        'ylc_sku' => $row['ylc_sku'] ?? null,
+                        'manitoba_barcodeupc' => $row['manitoba_barcodeupc'] ?? null,
+                        'ontario_barcodeupc' => $row['ontario_barcodeupc'] ?? null,
+                        'saskatchewan_barcodeupc' => $row['saskatchewan_barcodeupc'] ?? null,
+                        'link_to_product' => $row['link_to_product'] ?? null,
+                        'opening_inventory_units' => $row['opening_inventory_units'] ?? null,
+                        'quantity_purchased_units' => $row['quantity_purchased_units'] ?? null,
+                        'reductions_receiving_error_units' => $row['reductions_receiving_error_units'] ?? null,
+                        'returns_from_customers_units' => $row['returns_from_customers_units'] ?? null,
+                        'other_additions_units' => $row['other_additions_units'] ?? null,
+                        'quantity_sold_units' => $row['quantity_sold_units'] ?? null,
+                        'quantity_destroyed_units' => $row['quantity_destroyed_units'] ?? null,
+                        'quantity_lost_theft_units' => $row['quantity_lost_theft_units'] ?? null,
+                        'returns_to_supplier_units' => $row['returns_to_supplier_units'] ?? null,
+                        'other_reductions_units' => $row['other_reductions_units'] ?? null,
+                        'closing_inventory_units' => $row['closing_inventory_units'] ?? null,
+                        'report_id' => $this->reportId,
+                        'location' => $this->location,
+                        'retailer_id' => $this->retailerId, // Include retailer ID
+                        'lp_id' => $this->lpId,
+                    ]);
+
+                    $diagnosticReport->save(); // Save to get the ID
+                }
+            }
+        }
+
+        if ($report->province == 'AB' || $report->province == 'Alberta') {
+            if ($row['aglc_sku'] != null || $row['product_name'] != null || $row['manitoba_barcode_upc'] != null) {
+                if (($row['aglc_sku'] != '*')) {
+                    $diagnosticReport = new CovaDiagnosticReport([
+                        'product_name' => $row['product_name'] ?? null,
+                        'type' => $row['type'] ?? null,
+                        'aglc_sku' => $row['aglc_sku'] ?? null,
+                        'new_brunswick_sku' => $row['new_brunswick_sku'] ?? null,
+                        'ocs_sku' => $row['ocs_sku'] ?? null,
+                        'ylc_sku' => $row['ylc_sku'] ?? null,
+                        'manitoba_barcodeupc' => $row['manitoba_barcodeupc'] ?? null,
+                        'ontario_barcodeupc' => $row['ontario_barcodeupc'] ?? null,
+                        'saskatchewan_barcodeupc' => $row['saskatchewan_barcodeupc'] ?? null,
+                        'link_to_product' => $row['link_to_product'] ?? null,
+                        'opening_inventory_units' => $row['opening_inventory_units'] ?? null,
+                        'quantity_purchased_units' => $row['quantity_purchased_units'] ?? null,
+                        'reductions_receiving_error_units' => $row['reductions_receiving_error_units'] ?? null,
+                        'returns_from_customers_units' => $row['returns_from_customers_units'] ?? null,
+                        'other_additions_units' => $row['other_additions_units'] ?? null,
+                        'quantity_sold_units' => $row['quantity_sold_units'] ?? null,
+                        'quantity_destroyed_units' => $row['quantity_destroyed_units'] ?? null,
+                        'quantity_lost_theft_units' => $row['quantity_lost_theft_units'] ?? null,
+                        'returns_to_supplier_units' => $row['returns_to_supplier_units'] ?? null,
+                        'other_reductions_units' => $row['other_reductions_units'] ?? null,
+                        'closing_inventory_units' => $row['closing_inventory_units'] ?? null,
+                        'report_id' => $this->reportId,
+                        'location' => $this->location,
+                        'retailer_id' => $this->retailerId, // Include retailer ID
+                        'lp_id' => $this->lpId,
+                    ]);
+
+                    $diagnosticReport->save(); // Save to get the ID
+                }
+            }
+        }
 
         // Store the ID for later use
         $this->diagnosticReportId = $diagnosticReport->id;
